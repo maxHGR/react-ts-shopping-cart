c94ad80360c5ce717473e96864441d007b3e4a34
maxHGR <93156633+maxHGR@users.noreply.github.com>
2022-09-07 21:34:58 +0200

    1 file modified, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import './style.css';
 
-export default function App() {
+
+const App = () => {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello</h1>
     </div>
   );
 }
+
+export default App
